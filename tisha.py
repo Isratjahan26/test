276a64b648a85933990dcd1ac1227cc5fd99fbad
Isratjahan26 <adittijahan@gmail.com>
2021-09-05 11:43:50 +0600

    2nd version

--- tisha.py
+++ tisha.py
@@ -1,7 +1,7 @@
 '''
 Type casting AS STRING
 '''
-num1=(input("Enter the 1st number:"))
-num2=(input("Enter the 2nd number:"))
+num1=int(input("Enter the 1st number:"))
+num2=int(input("Enter the 2nd number:"))
 result=num1+num2
 print(result)
